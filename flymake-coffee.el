884b3293677e892d8deae904966e4f788b2455f1
Steve Purcell <steve@sanityinc.com>
2012-11-07 11:11:04 +0000

    Include "warn"/"error" in captured message, so they can be displayed differently
    Fixes #2

--- flymake-coffee.el
+++ flymake-coffee.el
@@ -27,7 +27,7 @@
     ("^SyntaxError: In \\([^,]+\\), \\(.+\\) on line \\([0-9]+\\)" 1 3 nil 2)
     ;; coffeelint
     ("SyntaxError: \\(.*\\) on line \\([0-9]+\\)" nil 2 nil 1)
-    ("\\(.+\\),\\([0-9]+\\),\\(?:warn\\|error\\),\\(.+\\)" 1 2 nil 3)))
+    ("\\(.+\\),\\([0-9]+\\),\\(\\(warn\\|error\\),.+\\)" 1 2 nil 3)))
 
 (defun flymake-coffee-command (filename)
   "Construct a command that flymake can use to check coffeescript source."
