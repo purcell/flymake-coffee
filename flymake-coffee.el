5fc6e97ecd62ac586d4932278347c75a3f7dde95
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2014-08-09 11:28:04 +0200

    Updated regexp to support new error format.

--- flymake-coffee.el
+++ flymake-coffee.el
@@ -38,6 +38,7 @@ Must be a full path, so use `expand-file-name' if you want to use \"~\" etc."
     ("^SyntaxError: In \\([^,]+\\), \\(.+\\) on line \\([0-9]+\\)" 1 3 nil 2)
     ;; coffeelint
     ("SyntaxError: \\(.*\\) on line \\([0-9]+\\)" nil 2 nil 1)
+    ("\\(.*?\\):\\([0-9]+\\):\\([0-9]+\\): \\(error\\): \\(.*\\)" 1 2 3 5)
     ("\\(.+\\),\\([0-9]+\\)\\(?:,[0-9]*\\)?,\\(\\(warn\\|error\\),.+\\)" 1 2 nil 3)))
 
 (defun flymake-coffee-command (filename)
